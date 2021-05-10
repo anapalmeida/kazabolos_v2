9c422ba558d66897bac7161c2d14665249e6db1f
Ana Almeida <57116592+anapalmeida@users.noreply.github.com>
2021-05-10 18:40:15 -0300

    Container bug fixed, social media added, footer added, sticky menu added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,40 @@
+$(function() {
+    var header = document.getElementById("site-header-top");
+    var scrollNavbar = document.getElementById("scroll-navbar");
+    var sticky = header.offsetTop;
+
+    $(window).on('scroll', function () { 
+        if (window.pageYOffset > sticky) {
+            
+            if (screen.width <= 991 || screen.availWidth <= 991) {
+                header.classList.add("sticky");
+                header.classList.remove("onMoveHeader");
+                scrollNavbar.classList.add("d-none");
+                header.classList.remove("onMoveNavbar");
+                console.log("passou")
+            } else {
+                header.classList.add("sticky");
+                header.classList.add("onMoveHeader");
+                scrollNavbar.classList.remove("d-none");
+                header.classList.add("onMoveNavbar");
+            }
+
+        } else {
+            header.classList.remove("sticky");
+            header.classList.remove("onMoveHeader");
+            scrollNavbar.classList.add("d-none");
+            header.classList.remove("onMoveNavbar");
+        }
+    })
+});
+
 function animatedClose() {
     $('.animated-icon3').toggleClass('open');
     $('.mobile-menu').toggleClass('open');
+
 }
 
 $(function() {
     AOS.init();
-  });
+});
+
