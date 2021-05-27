91eb7a02d7f53acdf895a32822c0136dce00e438
Ana Almeida <57116592+anapalmeida@users.noreply.github.com>
2021-05-27 17:15:57 -0300

    PHP file added

---
+++ assets/php/delivery.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome =$_POST['nome'];
+$tel =$_POST['tel'];
+$cep =$_POST['cep'];
+$rua =$_POST['rua'];
+$numero =$_POST['numero'];
+$complemento =$_POST['complemento'];
+$bairro =$_POST['bairro'];
+$cidade =$_POST['cidade'];
+$uf =$_POST['uf'];
+
+?>
